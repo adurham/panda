3e0130e2981f0031739f8d00bd52d103a54d0dca
Eric Brown <eric@ebrown.net>
2023-07-03 21:23:47 -0600

    Allow cancel spamming for all GM CAM

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -219,7 +219,7 @@ static int gm_tx_hook(CANPacket_t *to_send) {
   }
 
   // BUTTONS: used for resume spamming and cruise cancellation with stock longitudinal
-  if ((addr == 481) && gm_pcm_cruise) {
+  if ((addr == 481) && (gm_hw == GM_CAM)) {
     int button = (GET_BYTE(to_send, 5) >> 4) & 0x7U;
 
     bool allowed_cancel = (button == 6) && cruise_engaged_prev;
