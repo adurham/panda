1f3f44c20365a7890da59ccfd1f429a65721a8f8
Kevin Smith <kevin@spleck.net>
2023-10-16 22:36:53 -0500

    initial batch of example marsmode scripts

---
+++ examples/marsmode/marsmode-speed-basic.py
@@ -0,0 +1,23 @@
+
+# marsmode-mediavolume-basic
+
+import binascii
+import time
+import random
+from panda import Panda
+
+p = Panda()
+
+p.set_can_speed_kbps(0,500)
+p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+
+while True:
+    try:
+        p.can_send(0x3c2,b"\x29\x55\x00\x3f\x00\x00\x00\x00",0) # speed down
+        time.sleep(0.3)
+        p.can_send(0x3c2,b"\x29\x55\x00\x01\x00\x00\x00\x00",0) # speed up
+        time.sleep(4+random.uniform(0,4))
+    except Exception as e:
+        print("Exception caught ",e)
+        time.sleep(3.2)
+
