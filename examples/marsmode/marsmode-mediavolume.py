0c82aecadd2934712c098c5e418e1e7267660bf3
Kevin Smith <kevin@spleck.net>
2024-04-14 16:09:26 -0500

    filtering unwanted data from second bus

--- examples/marsmode/marsmode-mediavolume.py
+++ examples/marsmode/marsmode-mediavolume.py
@@ -16,17 +16,18 @@ step_count = 0
 while True:
 	try:
 		can_recv = p.can_recv()
-		for address, _, dat, _ in can_recv:
-			if address == 0x528:
-				step_count = step_count + 1
-				car_clock = datetime.fromtimestamp(int(binascii.hexlify(dat),16))
-				print("Car Clock: ", car_clock)
-				if step_count == 7:
-					p.can_send(0x3c2,b"\x29\x55\x01\x00\x00\x00\x00\x00",0) # vol up
-				if step_count == 8:
-					p.can_send(0x3c2,b"\x29\x55\x3f\x00\x00\x00\x00\x00",0) # vol down
-					print("sent event")
-					step_count = 0
+		for address, _, dat, bus in can_recv:
+			if bus == 0:
+				if address == 0x528:
+					step_count = step_count + 1
+					car_clock = datetime.fromtimestamp(int(binascii.hexlify(dat),16))
+					print("Car Clock: ", car_clock)
+					if step_count == 7:
+						p.can_send(0x3c2,b"\x29\x55\x01\x00\x00\x00\x00\x00",0) # vol up
+					if step_count == 8:
+						p.can_send(0x3c2,b"\x29\x55\x3f\x00\x00\x00\x00\x00",0) # vol down
+						print("sent event")
+						step_count = 0
 	except Exception as e:
 		print("Exception caught ",e)
 		time.sleep(1.2)
@@ -35,4 +36,5 @@ while True:
 		p = Panda()
 
 		p.set_can_speed_kbps(0,500)
+		p.set_can_speed_kbps(1,500)
 		p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
