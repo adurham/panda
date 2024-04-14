497bc8fc779e1b8a2733d45f7f4c839f1a49f75e
Kevin Smith <kevin@spleck.net>
2024-04-13 23:48:34 -0500

    cleanup and additional detections with install script

--- examples/marsmode/marsmode-install.sh
+++ examples/marsmode/marsmode-install.sh
@@ -12,6 +12,11 @@ echo '----------------------------------------------------------------------'
 
 BIND=`dirname $0`
 
+# verbose flag
+if [ "q$V" == "q" ]; then
+	V=0
+fi
+
 # check for PiOS
 echo -n "Checking for approved distro... "
 if [ -f '/etc/apt/sources.list.d/raspi.list' ]
@@ -31,74 +36,144 @@ else
 	fi
 fi
 
-# check for root
-echo -n "Checking for root access... "
-sleep 1 && echo "SKIPPED" && sleep 1
-
 # install dependencies
 echo -n "Installing system dependencies... "
-sudo apt-get update >/dev/null 2>&1
-sudo apt-get install -y dfu-util gcc-arm-none-eabi python3-pip libffi-dev git scons screen >/dev/null 2>&1
-sleep 1 && echo "DONE" && sleep 1
+if [ $V == 1 ]; then
+	echo " "; echo " "
+	sudo apt-get update
+	sudo apt-get install -y dfu-util gcc-arm-none-eabi python3-pip libffi-dev git scons screen
+else
+	sudo apt-get update >/dev/null 2>&1
+	sudo apt-get install -y dfu-util gcc-arm-none-eabi python3-pip libffi-dev git scons screen >/dev/null 2>&1
+	sleep 1 && echo "OK" && sleep 1
+fi
 
 # grab the git checkout
 echo -n "Checking out spleck panda git repo... "
-git clone https://github.com/spleck/panda.git ~/panda >/dev/null 2>&1
-python -m venv ~/panda/ >/dev/null 2>&1
+if [ -d "panda/.git" ]; then
+	echo " SKIPPED"
+else
+	if [ $V == 1 ]; then
+		echo " "; echo " "
+		git clone https://github.com/spleck/panda.git ~/panda
+		python -m venv ~/panda/
+	else
+		git clone https://github.com/spleck/panda.git ~/panda >/dev/null 2>&1
+		python -m venv ~/panda/ >/dev/null 2>&1
+		sleep 1 && echo "OK" && sleep 1
+	fi
+fi
+
+# python local env
+echo -n "Setting up local python env... "
+if [ -f "panda/bin/python3" ]; then
+	echo " SKIPPED"
+else
+	if [ $V == 1 ]; then
+		echo " "; echo " "
+		python -m venv ~/panda/
+	else
+		python -m venv ~/panda/ >/dev/null 2>&1
+		sleep 1 && echo "OK" && sleep 1
+	fi
+fi
+
+
 export PATH=~/panda/bin:$PATH
 cd panda
-sleep 1 && echo "DONE" && sleep 1
 
 # install requirements
 echo -n "Installed app dependencies... "
-pip install -r requirements.txt >/dev/null 2>&1
-python setup.py install >/dev/null 2>&1
-sleep 1 && echo "DONE" && sleep 1
+if [ $V == 1 ]; then
+	echo " "; echo " "
+	pip install -r requirements.txt
+	python setup.py install
+else
+	pip install -r requirements.txt >/dev/null 2>&1
+	python setup.py install >/dev/null 2>&1
+	sleep 1 && echo "OK" && sleep 1
+fi
 
 # check for / setup udev rules
 echo -n "Checking udev configuration... "
-sudo tee /etc/udev/rules.d/11-panda.rules <<EOF >/dev/null
+if [ -f /etc/udev/rules.d/11-panda.rules ]; then
+	echo "SKIPPED"
+else
+	sudo tee /etc/udev/rules.d/11-panda.rules <<EOF >/dev/null
 SUBSYSTEM=="usb", ATTRS{idVendor}=="bbaa", ATTRS{idProduct}=="ddcc", MODE="0666"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="bbaa", ATTRS{idProduct}=="ddee", MODE="0666"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE="0666"
 EOF
-sudo udevadm control --reload-rules && sudo udevadm trigger
-sleep 1 && echo "DONE" && sleep 1
+	sudo udevadm control --reload-rules && sudo udevadm trigger
+	sleep 1 && echo "OK" && sleep 1
+fi
 
 # build custom panda firmware
 cd board
 echo -n "Building firmware... "
-scons -u >/dev/null 2>&1
-sleep 1 && echo "DONE" && sleep 1
+if [ $V == 1 ]; then
+	echo " "; echo " "
+	scons -u
+else
+	scons -u >/dev/null 2>&1
+	sleep 1 && echo "OK" && sleep 1
+fi
 
 # symlink hotfix for flashing
 for dir in ~/panda/lib/python*/site-packages/pandacan*; do ln -s ~/panda/board $dir/board; done
 
 echo -n "Recovery mode panda... "
+echo " "; echo " "
 ./recover.py
-sleep 1 && echo "DONE" && sleep 1
+if [ $? -ne 0 ]; then
+	echo "*** RECOVERY MODE ERROR *** exit code $?"
+	exit -1
+fi
+echo " "; echo " "
 
 echo -n "Flashing panda... "
+echo " "; echo " "
 ./flash.py
-sleep 1 && echo "DONE" && sleep 1
+if [ $? -ne 0 ]; then
+	echo "*** FLASH ERROR *** exit code $?"
+	exit -1
+fi
+echo " "; echo " "
 
 # add rc.local execution if not present
 echo -n 'Checking rc.local for startup... '
-grep -v ^exit /etc/rc.local >/tmp/.rcl
-echo screen -d -m -S mars /home/$USER/panda/examples/marsmode/marsmode-active.sh >>/tmp/.rcl
-echo exit 0 >>/tmp/.rcl
-cat /tmp/.rcl | sudo tee /etc/rc.local >/dev/null
-sleep 1 && echo DONE && sleep 1
+cnt=`grep marsmode /etc/rc.local | wc -l`
+if [ $cnt -gt 0 ]; then
+        echo "SKIPPED"
+else
+        echo "adding startup to rc.local"
+	grep -v ^exit /etc/rc.local >/tmp/.rcl
+	echo screen -d -m -S mars /home/$USER/panda/examples/marsmode/marsmode-active.sh >>/tmp/.rcl
+	echo exit 0 >>/tmp/.rcl
+	if [ $V == 1 ]; then
+		echo " "; echo " "
+		cat /tmp/.rcl | sudo tee /etc/rc.local
+	else
+		cat /tmp/.rcl | sudo tee /etc/rc.local >/dev/null
+		sleep 1 && echo OK && sleep 1
+	fi
+fi
 
 # run marsmode-active.sh to link default active script
 echo -n 'Setting default MarsMode script to marsmode-mediavolume-basic... '
-~/panda/examples/marsmode/marsmode-active.sh ~/panda/examples/marsmode/marsmode-mediavolume-basic.py >/dev/null
-sleep 1 && echo OK && sleep 1
+if [ $V == 1 ]; then
+	echo " "; echo " "
+	~/panda/examples/marsmode/marsmode-active.sh ~/panda/examples/marsmode/marsmode-mediavolume-basic.py
+else
+	~/panda/examples/marsmode/marsmode-active.sh ~/panda/examples/marsmode/marsmode-mediavolume-basic.py >/dev/null
+	sleep 1 && echo OK && sleep 1
+fi
 
 # add boot config to enable single cable for power+data
 echo dtoverlay=dwc2,dr_mode=host | sudo tee -a /boot/firmware/config.txt >/dev/null
 
 # done
+echo ' '
 echo '----------------------------------------------------------------------'
 echo ' '
 echo '        ** MarsMode install complete. Ready to GO! **'
